d2173e578b149e95d113b4fe826ef47d0fe41661
Rafal Manka <rafal@manka.info>
2019-02-16 21:33:50 +0400

    Cleanp imports

--- Tanks/Source/Tanks/SprungWheel.h
+++ Tanks/Source/Tanks/SprungWheel.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-// #include "Runtime/Engine/Classes/PhysicsEngine/PhysicsConstraintComponent.h"
 #include "SprungWheel.generated.h"
 
 class UPhysicsConstraintComponent;
